41d9c83dc0ccac9856df7fb0a143746ba90be11c
Fabio de Araujo <fabioaraujo1608@gmail.com>
2025-10-25 21:45:46 -0300

    Date corrections

--- reader/ocr_extraction.py
+++ reader/ocr_extraction.py
@@ -1,6 +1,6 @@
 import easyocr
-from .files_extraction import extract_files
-from .frame_extraction import extract_frames
+from files_extraction import extract_files
+from frame_extraction import extract_frames
 from typing import List, Any
 import pandas as pd
 from pathlib import Path
@@ -19,5 +19,8 @@ def run_ocr(path):
     replaced = [text[0].replace(" ", ""), text[1].replace(" ", "")]
     final_list = [frame[1], replaced[0], replaced[1]]
     data_list.append(final_list)
-
-  return pd.DataFrame(data_list)
+    df = pd.DataFrame(data_list, columns=["id", "data", "hora"])
+    df["data"] = pd.to_datetime(df["data"]).dt.strftime("%d/%m/%y")
+    df["hora"] = pd.to_datetime(df["hora"]).dt.strftime("%H:%M:%S")
+    df.sort_values(by=["data"])
+  return df
